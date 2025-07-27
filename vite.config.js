d2de7b4da38884b8f1b8acafa5568f9d3e3e77e2
N-Abhishek-S <nagargojeabhishek96@gmail.com>
2025-07-27 21:33:50 +0530

    vite.config.js error solved

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/Tesla_CarshworoomWeb/', // ✅ Important for GitHub Pages
-  plugins: [react()]
+ plugins: [react()],
+   build: {
+     outDir: 'dist',
+     emptyOutDir: true,
+     assetsInlineLimit: 0,
+     assetsDir: 'assets',  
+   },
+   server: {
+     historyApiFallback: true,
+   },
+   optimizeDeps: {
+     exclude: ['*.jpg', '*.png'], // Don't optimize external images
+   }
 });
